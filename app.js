179cfe63546752209f9273f95561c2b1a05cc4a2
Lucas Andrade <lucascorporation@live.com>
2023-08-08 00:00:45 -0300

    routes express

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(express.urlencoded({ extended: true })) /*Express can do it instead of bodyParser*/
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="post"> <input type="text" name="title"><button type="submit">Add a Product</button></form>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/product',(req,res,next)=>{
-  console.log(req.body);
-  res.redirect('/');;
-
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
