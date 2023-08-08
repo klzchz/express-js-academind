8d49ff4e4215b416c90543a68e322c718fb7326d
Lucas Andrade <lucascorporation@live.com>
2023-08-07 23:20:05 -0300

    adding body-parser package and parsing data with urlencoded

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(express.urlencoded({ extended: true })) /*Express can do it instead of bodyParser*/
 
 
 app.use('/add-product', (req, res, next) => {
