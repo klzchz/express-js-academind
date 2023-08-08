09c86f75463e7fea1dbd412869556182b9837c4d
Lucas Andrade <lucascorporation@live.com>
2023-08-08 00:10:01 -0300

    not found page

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const notFound = require('./routes/404');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use(notFound);
 
 app.listen(3000);
