8db4aaf83fb53f4ab58134591683d25a8e6632fc
Lucas Andrade <lucascorporation@live.com>
2023-08-07 23:10:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="post"> <input type="text" name="title"><button type="submit">Add a Product</button></form>');
+});
+
+app.use('/product',(req,res,next)=>{
+  console.log(req.body);
+  res.redirect('/');;
+
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
