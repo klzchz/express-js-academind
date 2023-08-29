44f16ea9d1448f97d0d2fb8d7bf0d241265f4e00
Lucas Andrade <lucascorporation@live.com>
2023-08-28 22:19:55 -0300

    melhorando a navegacao

--- routes/404.js
+++ routes/404.js
@@ -1,11 +1,11 @@
 const path = require('path')
 const  express = require('express');
-
 router  = express.Router();
+const rootDir = require('../util/path');
 
 router.use((req,res,next)=>{
     // res.status(404).send('<h1>Page Not Found</h1>');
-    res.sendFile(path.join(__dirname,'../','views','404.html'));
+    res.sendFile(path.join(rootDir,'views','404.html'));
 })
 
 module.exports = router;
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();const fs = require('fs');
+const rootDir =  require('../util/path');
 
 
 
   router.get('/add-product', (req, res, next) => {
     // res.send('<form action="/admin/add-product" method="post"> <input type="text" name="title"><button type="submit">Add a Product</button></form>');
-    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
   });
   
   router.post('/add-product',(req,res,next)=>{
