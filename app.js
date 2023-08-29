ef8802202012b3b5234a60b37a31cc15fa93b616
Lucas Andrade <lucascorporation@live.com>
2023-08-28 22:46:36 -0300

    wrap up

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFound = require('./routes/404');
+const path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
-
+app.use(express.static(path.join(__dirname,'public')))
 
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
