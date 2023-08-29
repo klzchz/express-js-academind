25a943f99358c46af3f3361d038925ba7456bef0
Lucas Andrade <lucascorporation@live.com>
2023-08-28 22:06:57 -0300

    mvc views

--- routes/404.js
+++ routes/404.js
@@ -1,10 +1,11 @@
+const path = require('path')
 const  express = require('express');
-
 
 router  = express.Router();
 
 router.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1>');
+    // res.status(404).send('<h1>Page Not Found</h1>');
+    res.sendFile(path.join(__dirname,'../','views','404.html'));
 })
 
 module.exports = router;
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const router = express.Router();
+const path = require('path');
+const express = require('express');
+const router = express.Router();const fs = require('fs');
+
+
 
   router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="post"> <input type="text" name="title"><button type="submit">Add a Product</button></form>');
+    // res.send('<form action="/admin/add-product" method="post"> <input type="text" name="title"><button type="submit">Add a Product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
   });
   
   router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
+    console.log(req.body.title)
+    fs.writeFileSync('message.text',req.body.title);
     res.redirect('/');;
   
   });
