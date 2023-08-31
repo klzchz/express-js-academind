024cd99f255ba77ce2e572ee77c2d6dcba332de0
Lucas Andrade <lucascorporation@live.com>
2023-08-31 00:50:14 -0300

    pug shop page

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,9 @@ const adminData = require('./admin');
 router.get('/', (req, res, next) => {
   // res.send('<h1>Hello from Express!</h1>');
   console.log(adminData.products);
+  let prods = adminData.products;
   // res.sendFile(path.join(rootDir,'views','shop.html'));
-  res.render('shop');
+  res.render('shop',{prods, docTitle: 'Shop'});
 });
 
 module.exports = router;
