aa89c2854728a4a08807df25eb812969daf9a489
Lucas Andrade <lucascorporation@live.com>
2023-09-27 22:34:34 -0300

    adding a product Model

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,6 @@
 
-const products = [];
+// const products = [];
+const Product  = require('../models/product');
 
 
 
@@ -17,15 +18,17 @@ exports.getAddProduct =  (req, res, next) => {
 
 exports.addProduct = (req,res,next)=>{
     console.log(req.body.title)
+    const product = new Product(req.body.title);
+    product.save();
     // fs.writeFileSync('message.text',req.body.title);
-    products.push({title: req.body.title});
+    // products.push({title: req.body.title});
     res.redirect('/');;
   
   };
 
 exports.getProducts = (req, res, next) => {
     // res.send('<h1>Hello from Express!</h1>');
-   
+    const products = Product.fetchAll();
     // res.sendFile(path.join(rootDir,'views','shop.html'));
     res.render('shop',{
       hasProducts:products.length > 0,
